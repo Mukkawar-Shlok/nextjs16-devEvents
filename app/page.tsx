8f05e9f0f130ebb1587007d2d6abbb90118f732d
Shlok Mukkawar <shlokmukkawar11@gmail.com>
2025-11-09 18:54:35 +0530

    Refactor event page to enable API data fetching
    - Uncommented API call to fetch events from the server.
    - Enhanced event mapping by specifying the IEvent type for better type safety.
    - Improved overall structure for clarity and maintainability.

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import ExplorerBtn from '../components/ExplorerBtn'
 import EventCard from '@/components/EventCard'
-import { events } from '@/lib/constants'
-import { IEvent } from '@/database';
 import { cacheLife } from 'next/cache';
+import { IEvent } from '@/database';
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 const page = async() => {
   'use cache';
   cacheLife('hours');
-  // const response = await fetch(`${BASE_URL}/api/events`);
-  // const {events} = await response.json();
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const {events} = await response.json();
   return (
     <section>
       <h1>The hub for anime events</h1>
@@ -20,7 +19,7 @@ const page = async() => {
         <h3>Featured Events</h3>
 
         <ul className="events">
-          {events && events.length > 0 && events.map((event) => (
+          {events && events.length > 0 && events.map((event: IEvent) => (
               <EventCard {...event} key={event.title}/>
           ))}
         </ul>
